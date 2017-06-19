165cfca47a8af525ea3f7c3bdb51fae9e2f7cb65
nervous16 <>
2017-06-19 15:28:17 +0300

    test

--- Tables/dbo.Users.sql
+++ Tables/dbo.Users.sql
@@ -4,9 +4,10 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Users] (
 		[id]          [int] NULL,
-		[AdSoyad]     [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Ad]          [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[Email]       [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[DTarihi]     [date] NULL
+		[DTarihi]     [date] NULL,
+		[Soyad]       [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Users] SET (LOCK_ESCALATION = TABLE)
